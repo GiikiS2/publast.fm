923900a90777a200d02b12503b11ef6d36695968
Giiki_S2 <guibollellalongo@gmail.com>
2024-07-26 19:06:21 -0300

    awa

--- src/pages/api/[artist]/[track]/info.ts
+++ src/pages/api/[artist]/[track]/info.ts
@@ -11,20 +11,8 @@ export async function GET({ params, request }: { params: any, request: any }) {
       statusText: 'Track or artist missing.',
     });
   }
-
-  const parsedUrl = new URL(request.url);
-
-  const period = parsedUrl.searchParams.get('period');
-  const limit = parsedUrl.searchParams.get('limit');
-
-
+  
   const newUrl = new URL(`http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${process.env.LastKey}&artist=${params.artist}&track=${params.track}&format=json`)
-  if (period) {
-    newUrl.searchParams.append('period', period);
-  }
-  if (limit) {
-    newUrl.searchParams.append('limit', limit);
-  }
 
   var fetched = await axios.get(newUrl.toString())
 
--- src/pages/api/[user]/toptracks.ts
+++ src/pages/api/[user]/toptracks.ts
@@ -4,7 +4,7 @@ export const prerender = false;
 
 
 
-export async function GET({ params }: { params: any }) {
+export async function GET({ params, request }: { params: any, request: any }) {
 
   if (!params.user) {
     return new Response(null, {
@@ -13,8 +13,21 @@ export async function GET({ params }: { params: any }) {
     });
   }
 
-  var fetched = await axios.get(`https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${params.user}&api_key=${process.env.LastKey}&format=json`) //&period=7day&limit=6
+  const parsedUrl = new URL(request.url);
 
+  const period = parsedUrl.searchParams.get('period');
+  const limit = parsedUrl.searchParams.get('limit');
+
+  //&period=7day&limit=6
+  const newUrl = new URL(`https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${params.user}&api_key=${process.env.LastKey}&format=json`)
+  if (period) {
+    newUrl.searchParams.append('period', period);
+  }
+  if (limit) {
+    newUrl.searchParams.append('limit', limit);
+  }
+  var fetched = await axios.get(newUrl.toString())
+  
   return new Response(JSON.stringify(fetched.data), {
     headers: {
       "Content-Type": "application/json",
